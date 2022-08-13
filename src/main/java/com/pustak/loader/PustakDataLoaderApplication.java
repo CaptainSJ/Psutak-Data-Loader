6e65bb01126a25d7a28c3ffa1e16b5aed6780cb8
Saurabh Joshi <i.saurabhjoshi@hotmail.com>
2022-08-14 01:35:10 +0530

    added config to connect Cassendra

--- src/main/java/com/pustak/loader/PustakDataLoaderApplication.java
+++ src/main/java/com/pustak/loader/PustakDataLoaderApplication.java
@@ -1,13 +1,29 @@
 package com.pustak.loader;
 
+import java.nio.file.Path;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import com.pustak.loader.connection.DataStaxAstraProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(DataStaxAstraProperties.class)
 public class PustakDataLoaderApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PustakDataLoaderApplication.class, args);
+	}
+	
+	@Bean
+	public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties properties)
+	{
+		Path bundle = properties.getSecureConnectBundle().toPath();
+		return builder -> builder.withCloudSecureConnectBundle(bundle);
+		
 	}
 
 }
