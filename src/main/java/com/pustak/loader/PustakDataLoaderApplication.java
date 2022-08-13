2a16418cdb0f7ffdf85a8022367660ac45e36ed9
Saurabh Joshi <i.saurabhjoshi@hotmail.com>
2022-08-13 23:13:17 +0530

    init commit

---
+++ src/main/java/com/pustak/loader/PustakDataLoaderApplication.java
@@ -0,0 +1,13 @@
+package com.pustak.loader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PustakDataLoaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PustakDataLoaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pustak/loader/PustakDataLoaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pustak.loader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PustakDataLoaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
